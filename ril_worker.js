8cd88715f57f79bab356000e7b27cac2e4a5f4a9
Philipp von Weitershausen <philipp@weitershausen.de>
2011-12-16 03:39:57 -0800

    make sure ferjm gets credit

--- ril_worker.js
+++ ril_worker.js
@@ -21,6 +21,7 @@
  * Contributor(s):
  *   Kyle Machulis <kyle@nonpolynomial.com>
  *   Philipp von Weitershausen <philipp@weitershausen.de>
+ *   Fernando Jimenez <ferjmoreno@gmail.com>
  *
  * Alternatively, the contents of this file may be used under the terms of
  * either the GNU General Public License Version 2 or later (the "GPL"), or
